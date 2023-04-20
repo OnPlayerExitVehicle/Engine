066d004f6eea93bca580f30d872a801131dcd3a7
Ali Nazif Koca <35431958+OnPlayerExitVehicle@users.noreply.github.com>
2023-04-20 04:27:05 +0300

    feat: add engine class

--- Core/include/Engine.h
+++ Core/include/Engine.h
@@ -1,16 +1,21 @@
-//#pragma once
-//#include "Scene.h"
-//#include <string>
-//
-//class Engine
-//{
-//private:
-//	std::unique_ptr<Scene> currentScene;
-//	
-//	static std::unique_ptr<Engine> instance;
-//
-//	Engine();
-//
-//public:
-//	static std::shared_ptr<Engine> QuerryEngine();
-//};
+#pragma once
+#include "Scene.h"
+#include <string>
+#include "GLFW/include/GLFW/glfw3.h"
+#include <thread>
+
+class Engine
+{
+private:
+	GLFWwindow* window;
+	Scene* scene;
+	std::thread loopThread;
+
+	void InitWindow(int windowWidth, int windowHeight);
+	void InitGraphics();
+	void InitDefaultBehaviour();
+
+public:
+	Engine(int windowWidth, int windowHeight);
+	void EngineLoop();
+};
--- Core/src/Engine.cpp
+++ Core/src/Engine.cpp
@@ -1,15 +1,60 @@
-//#include "Engine.h"
-//
-//std::unique_ptr<Engine> Engine::instance;
-//
-//Engine::Engine() : currentScene(std::make_unique<Scene>()) { }
-//
-//std::shared_ptr<Engine> Engine::QuerryEngine()
-//{
-//	if (instance == nullptr)
-//	{
-//		instance = std::make_unique<Engine>();
-//	}
-//
-//	return std::move(instance);
-//}
+#include "Engine.h"
+#include "PlaneCollider.h"
+#include "Input.h"
+#include "Camera.h"
+#include "TestComponent.h"
+#include "HTime.h"
+#include "Rigidbody.h"
+
+Engine::Engine(int windowWidth, int windowHeight)
+{
+	srand(NULL);
+	InitWindow(windowWidth, windowHeight);
+	InitGraphics();
+
+	scene = new Scene(window);
+	Input::Initialize(window);
+
+	InitDefaultBehaviour();
+}
+
+void Engine::InitWindow(int windowWidth, int windowHeight)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window = glfwCreateWindow(windowWidth, windowHeight, std::format("Engine ({} bit)", sizeof(void*) * 8).c_str(), nullptr, nullptr);
+	if (window == nullptr) throw std::exception("Cannot create window!");
+}
+
+void Engine::InitGraphics()
+{
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void Engine::InitDefaultBehaviour()
+{
+	auto plane = scene->CreateObject("Plane");
+	plane->AddComponent<PlaneCollider>();
+	plane->AddComponent<Rigidbody>();
+
+	auto camera = scene->CreateObject("Camera", Vector3(0.0f, -3.0f, -10.0f));
+	camera->AddComponent<Camera>();
+	camera->AddComponent<TestComponent>();
+}
+
+void Engine::EngineLoop()
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		Time::FrameStart();
+
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		scene->Process();
+		glfwSwapBuffers(window);
+	}
+}
