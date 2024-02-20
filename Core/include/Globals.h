1e9263c1487dba09ff3dcb614c1a794841a122b8
AliNazifKoca <35431958+OnPlayerExitVehicle@users.noreply.github.com>
2024-02-20 22:28:43 +0300

    fix: before merge conflicts fix

--- Core/include/Globals.h
+++ Core/include/Globals.h
@@ -1,12 +1,17 @@
 #pragma once
 
-constexpr int WINDOW_WIDTH  = 1280;
-constexpr int WINDOW_HEIGHT = 960;
+constexpr inline int DEFAULT_WINDOW_WIDTH = 960;
+constexpr inline int DEFAULT_WINDOW_HEIGHT = 540;
+
+inline int WINDOW_WIDTH  = DEFAULT_WINDOW_WIDTH;
+inline int WINDOW_HEIGHT = DEFAULT_WINDOW_HEIGHT;
 
 #define _STD_ std::
 #define _STRING_R_ _STD_ string
 #define _STRING_CR_ const _STD_ string&
 #define DEBUG_LOG(str) _STD_ cout << str << _STD_ endl
+#define DEBUG_FORMAT(str, ...) _STD_ cout << _STD_ format(str, __VA_ARGS__) << _STD_ endl
+
 #if WIN32
     #define CORE_DIRECTORY "../../Core"
 #else
@@ -16,3 +21,4 @@ constexpr int WINDOW_HEIGHT = 960;
 ///
 #define TEXTURES_DIRECTORY CORE_DIRECTORY"/textures/"
 #define SHADERS_DIRECTORY CORE_DIRECTORY"/shaders/"
+#define OBJECTS_DIRECTORY CORE_DIRECTORY"/objects/"
